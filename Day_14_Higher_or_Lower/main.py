84a53aa5a90177b885cfc5fb3ebba978c8c4c979
Ahmed Khadawardi <ahes0001@student.monash.edu>
2023-11-27 12:34:52 +1100

    fix format to follow PEP 8 Style Guide

--- Day_14_Higher_or_Lower/main.py
+++ Day_14_Higher_or_Lower/main.py
@@ -3,36 +3,32 @@ from game_data import data
 import random
 import os
 
-376656060079931
-376656060079931
-376656060079931
-12/26
-12/26
-12/26
 
-6959
-6959
-6959
+def return_print(obj, letter):
+    return f"Compare {letter}: {obj['name']}, a {obj['description']}, from {obj['country']}."
 
-def returnPrint(obj, letter):
-    return (f"Compare {letter}: {obj['name']}, a {obj['description']}, from {obj['country']}.")
 
-def PrintResults(score):
+def print_results(score):
     print(logo)
     print(f"Sorry, that's wrong. Final score: {score}")
-def Correct(answer):
+
+
+def correct(answer):
     print(answer['name'])
     print(a['name'])
     if a['follower_count'] > b['follower_count']:
         if answer['name'] == a['name']:
             return True
-        else: return False
+        else:
+            return False
     else:
         if answer['name'] == b['name']:
             return True
-        else: return False
-        
-correct = ""
+        else:
+            return False
+
+
+one_correct = ""
 not_lost = True
 random_data = random.choice(data)
 a = random_data
@@ -40,31 +36,30 @@ data.remove(random_data)
 Score = 0
 
 while not_lost:
-    
-
     random_data = random.choice(data)
     b = random_data
     data.remove(random_data)
     print(logo)
-    print(f"{correct}Curernt Score: {Score}")
-    print(returnPrint(a, "A"))
+    print(f"{one_correct}Current Score: {Score}")
+    print(return_print(a, "A"))
     print(vs)
-    print(returnPrint(b, "B"))
+    print(return_print(b, "B"))
     
     answer = input("Who has more followers? Type 'A' or 'B': ")
     if answer.strip().lower() == a:
         answer = a
-    else: answer = b
-    
+    else:
+        answer = b
 
-    if Correct(answer):
-        Score+=1
+    if correct(answer):
+        Score += 1
         a = b
-    else:not_lost = False
+    else:
+        not_lost = False
 
-    if correct == "":
-        correct = "You're right! "
+    if one_correct == "":
+        one_correct = "You're right! "
 
     os.system('cls')
 
-PrintResults(Score)
+print_results(Score)

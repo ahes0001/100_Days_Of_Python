95cb2e35b2042e721b56caa332a14e9c66c1b995
Ahmed Khadawardi <ahes0001@student.monash.edu>
2023-12-04 11:06:56 +1100

    create player

--- Day_23_Turtle_Crossing_Captone/player.py
+++ Day_23_Turtle_Crossing_Captone/player.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 100
 FINISH_LINE_Y = 280
 
 
@@ -12,9 +12,11 @@ class Player(Turtle):
         self.setheading(90)
         self.color("blue")
         self.goto(STARTING_POSITION)
+        self.level = 0.1
 
     def move_forward(self):
         self.forward(10)
 
     def reset(self):
         self.goto(STARTING_POSITION)
+        self.level *= 0.7

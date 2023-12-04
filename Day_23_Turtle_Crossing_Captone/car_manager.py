7207cf0d9aead82baf6e0010255b4d2cceb15976
Ahmed Khadawardi <ahes0001@student.monash.edu>
2023-12-04 11:07:49 +1100

    create car

--- Day_23_Turtle_Crossing_Captone/car_manager.py
+++ Day_23_Turtle_Crossing_Captone/car_manager.py
@@ -1,7 +1,31 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+X_COR_START_POS = 300
+X_COR_END_POS = -300
+Y_COR_TOP_SPAWN_BOUNDARY = 240
+Y_COR_BOT_SPAWN_BOUNDARY = -240
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+
+    def move(self, car):
+        car.forward(STARTING_MOVE_DISTANCE)
+
+    def create_car(self):
+        rand_num = random.randint(1,6)
+        if rand_num == 6:
+            car = Turtle()
+            car.color(COLORS[random.randint(0, 5)])
+            car.penup()
+            car.shape("square")
+            car.setheading(180)
+            car.shapesize(1, 2)
+            car.setpos(X_COR_START_POS, random.randint(Y_COR_BOT_SPAWN_BOUNDARY, Y_COR_TOP_SPAWN_BOUNDARY))
+            self.all_cars.append(car)
+
+
